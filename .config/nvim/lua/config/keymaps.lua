d53a4b9cd7ed8c731f6bb30d7a0eeb5019748699
Vengo Langoolagni <vengolan@gmail.com>
2023-06-18 14:45:34 +0530

    Codeium plugin added
    Shortcuts

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+vim.keymap.set("i", "<C-g>", function()
+  return vim.fn["codeium#Accept"]()
+end, { expr = true })
+
+vim.keymap.set("i", "<C-x>", function()
+  return vim.fn["codeium#Clear"]()
+end, { expr = true })
 
 vim.keymap.set("i", "jk", "<Esc>", {})
